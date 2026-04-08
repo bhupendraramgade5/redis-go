4fb97b3751be3cbad7a678192d8e7e780eaa0d56
bhupendraramgade5 <bhupendraramgade5@gmail.com>
2026-04-08 20:17:11 +0000

    SET and GET command added

--- app/commands.go
+++ app/commands.go
@@ -1,18 +1,21 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-// May be there is a method which doesnt require the use of arity in future 
-// But to write implementation of that method we were still dependent on the 
+// May be there is a method which doesnt require the use of arity in future
+// But to write implementation of that method we were still dependent on the
 // Arity function
-// thus by segregatin the interface we can now directky use the methods that we actually 
+// thus by segregatin the interface we can now directky use the methods that we actually
 // need, and saving efforts in writing methods that are unneccesary
 
 type Command interface{
 	Execute(args []string) string
 }
 
-type Arity interface{
+type ArityChecker interface{
 	Arity() int
 }
 
@@ -35,17 +38,34 @@ func (echo EchoCommand) Arity() int {
 	return 2
 }
 
+var internalmap map[string]string
 
-type SetCommand struct{}
-func (set SetCommand) Execute(args[] string) string{
-
+// type Server struct {
+// 	store map[string]string
+// }
+type SetCommand struct{
 }
-
+func (set SetCommand) Execute(args[] string) string{
+	internalmap[args[1]]=value
+	return "+OK\r\n"
+}
+type GetCommand struct{}
+func (get GetCommand) Execute(args [] string)string{
+	value, ok:=internalmap[args[1]]
+	if ok {
+		var response string 
+		fmt.Sprintf(response, "$%d\r\n%s\r\n", len(value),value) // RESP response type bulk string 
+		return response 
+	}else{
+		return "-1\r\n" // Null Bulk String :: special type
+	}
+}
 
 var commands = map[string]Command{
 	"PING": PingCommand{},
 	"ECHO": EchoCommand{},
 	"SET": SetCommand{},
+	"GET" : GetCommand{},
 }
 
 func handleCommand(args []string )string {
@@ -53,12 +73,17 @@ func handleCommand(args []string )string {
 		return "-ERR unknown command\r\n"
 	}
 	command:=strings.ToUpper(args[0])
-	handler, ok:=commands[command]
-	if !ok{
-		return "-ERR unknown command\r\n"
+	// handler, ok:=commands[command]
+
+	handler := commands[command]
+
+	if arityCmd, ok := handler.(ArityChecker); ok {
+		if len(cmd) != arityCmd.Arity() {
+			return "-ERR wrong number of arguments\r\n"
+		}
 	}
-	if len(args)!=handler.Arity(){
-		return "-ERR wrong number of arguments\r\n"
-	}
+
+	return handler.Execute(cmd)
+
 	return handler.Execute(args)
 }
