374a162757a084d6dd40a7d705310a53cfe4ba38
bhupendraramgade5 <bhupendraramgade5@gmail.com>
2026-04-08 20:26:13 +0000

    SET and GET command added : Bug solved

--- app/main.go
+++ app/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-
+	var internalmap = make(map[string]string)
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
